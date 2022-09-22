d5745fce95a4887db30d302526b3e11c68778b2d
miono <mercy.promo.2001@gmail.com>
2022-09-22 15:49:51 +0300

    load

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,7 +11,5 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
-console.log(firebaseConfig)
-
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
